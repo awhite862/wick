ba1e13364b7f64ca027db3b8c53bb5ef8288fa31
awhite862 <whiteaf862@gmail.com>
2021-04-13 15:14:28 -0700

    Fix planned failure

--- wick/tests/test_test.py
+++ wick/tests/test_test.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestTest(unittest.TestCase):
     def test_framework(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
