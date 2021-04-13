0e15cb3ec707a0ebf268a393a2c714b31557c119
awhite862 <whiteaf862@gmail.com>
2021-04-13 15:12:47 -0700

    Add failing test for github actions

--- wick/tests/test_test.py
+++ wick/tests/test_test.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestTest(unittest.TestCase):
     def test_framework(self):
-        self.assertTrue(True)
+        self.assertTrue(False)
 
 if __name__ == '__main__':
     unittest.main()
